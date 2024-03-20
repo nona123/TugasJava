207b6714d8a212ef29c8b529249d25d49062ace8
nelsi <silabannelsi@gmail.com>
2024-03-20 18:47:31 +0700

    practical assesment 4

--- src/test/java/FolderTest/BaseTest.java
+++ src/test/java/FolderTest/BaseTest.java
@@ -1,2 +1,37 @@
-package FolderTest;public class BaseTest {
+package FolderTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    // open connection local
+    public ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public ThreadLocal<WebDriverWait> explicitWait = new ThreadLocal<>();
+
+    //Annotation
+    //Before (Open Browser, Additional Open Website or url)
+    @BeforeMethod
+    public void openBrowser(){
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+
+        // Getter and Setter untuk buka browser dan url
+        driver.set(new FirefoxDriver(options));
+        driver.get().manage().window().maximize();// maximize window
+        driver.get().get("https://www.demoblaze.com/");
+        explicitWait.set(new WebDriverWait((driver.get()), Duration.ofSeconds(120)));
+    }
+
+    //After (Close Browser atau close connection)
+    @AfterMethod
+    public void closeBrowser(){
+        driver.get().quit();
+    }
 }
--- src/test/java/FolderTest/GetProduct.java
+++ src/test/java/FolderTest/GetProduct.java
@@ -1,2 +1,52 @@
-package FolderTest;public class GetProduct {
+package FolderTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetProduct extends BaseTest {
+    @Test
+    public void getPrice() {
+        WebElement LaptopLink = driver.get().findElement(By.xpath("//a[.='Laptops']"));
+        LaptopLink.click();
+
+        WebElement Macbook = explicitWait.get().until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//a[.='MacBook air']")));
+        Macbook.click();
+
+        // Assertion, price visible and equal to $700
+        WebElement MacPrice = explicitWait.get().until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h3[@class='price-container']")));
+
+        Assert.assertTrue(MacPrice.getText().contains("$700"), "Price does not match");
+    }
+
+    @Test
+    public void testSignUp() {
+        driver.get().findElement(By.xpath("//a[.='Sign up']")).click();
+        explicitWait.get().until(ExpectedConditions.visibilityOfElementLocated(By.id("signInModal")));
+
+        driver.get().findElement(By.id("sign-username")).sendKeys("Fuy");
+        driver.get().findElement(By.id("sign-password")).sendKeys("hohoho123");
+        driver.get().findElement(By.xpath("//button[.='Sign up']")).click();
+
+        Alert alert = explicitWait.get().until(ExpectedConditions.alertIsPresent());
+        String alertMessage = alert.getText();
+        Assert.assertTrue(alertMessage.equals("Sign up successful.") || alertMessage.equals("This user already exist."), "Alert message was not as expected: " + alertMessage);
+        alert.accept();
+    }
+
+    @Test(priority = 2)
+    @Description("Test Description: add to cart")
+    public void testAddItem() {
+        WebElement addtoCart = driver.get().findElement(By.xpath("//a[.='Add to cart']"));addtoCart.click();
+        Alert alert = explicitwait.get().until(ExpectedConditions.alertIsPresent());
+        String alertMessage = alert.getText();
+        Assert.assertTrue(alertMessage.equals("Product added") || alertMessage.equals("error"), "Alert message was not as expected: " + alertMessage);
+        alert.accept();
+    }
 }
